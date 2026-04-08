7aa612043109dc52dd562f2bfd19e7621688382f
joeljoseph-riv21 <joeljosephrival@gmail.com>
2026-04-09 00:00:03 +0530

    Performing a linear search with break statement to check if a number is in list

---
+++ linear_search.py
@@ -0,0 +1,8 @@
+#Goal: To find a number that exists in the list here the number is 21
+list = [10, 21, 35, 47, 59, 32, 2, 23 ,17]
+target = 21
+
+for i in range(len(list)):
+    if list[i] == target:
+        print(f' Yes,the number {target} is in the list and it was found in the index {i}')
+        break
